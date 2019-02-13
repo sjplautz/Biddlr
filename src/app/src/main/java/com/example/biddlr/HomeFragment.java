8b852daa4436bafb54b455cd9ba6501f6af300e2
sjplautz <plautzstephen@gmail.com>
2019-02-13 13:57:13 -0600

    Added gmaps Api, updated layout of HomeFragment to include recyclerView and map. Updated HomeFragment implementation to dynamically load Map Fragment.

--- src/app/src/main/java/com/example/biddlr/HomeFragment.java
+++ src/app/src/main/java/com/example/biddlr/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +24,10 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Fragment mapFragment = new MapFragment();
+        FragmentTransaction trans = getChildFragmentManager().beginTransaction();
+        trans.add(R.id.mapView, mapFragment).commit();
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
