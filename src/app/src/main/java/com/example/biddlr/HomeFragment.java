fc2d3e0015c877d1a04c35b4515e2f051c7a81b3
sjplautz <plautzstephen@gmail.com>
2019-02-18 22:55:12 -0600

    Adding profile page UI with all elements except for reviews

--- src/app/src/main/java/com/example/biddlr/HomeFragment.java
+++ src/app/src/main/java/com/example/biddlr/HomeFragment.java
@@ -1,7 +1,5 @@
 package com.example.biddlr;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
--- src/app/src/main/java/com/example/biddlr/ProfileFragment.java
+++ src/app/src/main/java/com/example/biddlr/ProfileFragment.java
@@ -1,16 +1,34 @@
 package com.example.biddlr;
 
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Job;
 
 public class ProfileFragment extends Fragment {
-    public static ProfileFragment newInstance() {
-        ProfileFragment fragment = new ProfileFragment();
-        return fragment;
-    }
+
+    private List<Job> jobList;
+    private RecyclerView recycler;
+    private JobListAdapter adapter;
+    private TextView bio;
+
+    public static ProfileFragment newInstance() { return new ProfileFragment(); }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +38,88 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        jobList = new ArrayList<>();
+        adapter = new JobListAdapter(jobList);
+
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //create handle for rating bar and set start to 4/5 default rating
+        RatingBar rating = (RatingBar) v.findViewById(R.id.userRatingBar);
+        rating.setNumStars(5);
+
+        //setting the profile image resource to default baseline image
+        ImageView imgProfile = (ImageView) v.findViewById(R.id.DefaultProfileImage);
+        imgProfile.setImageResource(R.drawable.baseline_person_24);
+
+        //setting the padding inside the bio view
+        //bio = v.findViewById(R.id.ProfileBio);
+        //bio.setPadding(15,5,15,5);
+
+        //Sets the layout for the recycler view to inflate with
+        recycler = v.findViewById(R.id.completedJobsRecycler);
+
+        //Set the layout parameters
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+        recycler.setLayoutManager(manager);
+        recycler.setItemAnimator(new DefaultItemAnimator());
+        recycler.setAdapter(adapter);
+
+        recycler.addOnItemTouchListener(new JobListTouchListener(getContext(), recycler, new JobListTouchListener.ClickListener() {
+            @Override
+            public void onClick(View v, int pos) {
+                Job job = jobList.get(pos);
+                Fragment jobFrag = JobViewFragment.newInstance(job);
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction trans = manager.beginTransaction();
+                trans.replace(R.id.frameNull, jobFrag);
+                trans.addToBackStack(null);
+                trans.commit();
+            }
+
+            @Override
+            public void onLongClick(View v, int pos) {
+
+            }
+        }));
+
+        //populate the prefab data for demo
+        prepareSampleData();
+
         return v;
+    }
+
+    private void prepareSampleData() {
+        Job j1 = new Job(0, "This is the first job", null, 0, "job1", "123 Main St.", LocalDateTime.now().plusHours(1), 20.0);
+        jobList.add(j1);
+
+        Job j2 = new Job(0, "This is the second job", null, 0, "job2", null, LocalDateTime.now().plusHours(1), 999.0);
+        jobList.add(j2);
+
+        Job j3 = new Job(0, "This is the first job", null, 0, "job1", "Somewhere", LocalDateTime.now().plusHours(1), 20.0);
+        jobList.add(j3);
+
+        Job j4 = new Job(0, "This is the second job", null, 0, "job2", "Here", LocalDateTime.now().plusHours(1), 999.0);
+        jobList.add(j4);
+
+        Job j5 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
+        jobList.add(j5);
+
+        Job j6 = new Job(0, "This is the second job", null, 0, "job2", "There", LocalDateTime.now().plusHours(1), 999.0);
+        jobList.add(j6);
+
+        Job j7 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
+        jobList.add(j7);
+
+        Job j8 = new Job(0, "This is the second job", null, 0, "job2", "Anywhere", LocalDateTime.now().plusHours(1), 999.0);
+        jobList.add(j8);
+
+        Job j9 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
+        jobList.add(j9);
+
+        Job j10 = new Job(0, "This is the second job", null, 0, "job2", null, LocalDateTime.now().plusHours(1), 999.0);
+        jobList.add(j10);
+
+        adapter.notifyDataSetChanged();
     }
 }
