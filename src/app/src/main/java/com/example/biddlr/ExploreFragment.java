8390566e1b1d87167bd155e8294396b0fea7e127
Trevor Roest <twroest@crimson.ua.edu>
2019-02-18 23:00:19 -0600

    Added job creation page

--- src/app/src/main/java/com/example/biddlr/ExploreFragment.java
+++ src/app/src/main/java/com/example/biddlr/ExploreFragment.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import classes.DatabaseManager;
 import classes.Job;
 
 public class ExploreFragment extends Fragment {
@@ -35,7 +36,7 @@ public class ExploreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        jobList = new ArrayList<>();
+        jobList = DatabaseManager.getJobs();
         View v = inflater.inflate(R.layout.fragment_explore, container, false);
 
         adapter = new JobListAdapter(jobList);
@@ -62,41 +63,11 @@ public class ExploreFragment extends Fragment {
             }
         }));
 
-        prepareSampleData();
+        //prepareSampleData();
         return v;
     }
 
     private void prepareSampleData() {
-        Job j1 = new Job(0, "This is the first job", "Hello. I posted a job here. Take a look if you please.", 0, "job1", "123 Main St.", LocalDateTime.now().plusHours(1), 20.0);
-        jobList.add(j1);
-
-        Job j2 = new Job(0, "This is the second job", null, 0, "job2", null, LocalDateTime.now().plusHours(1), 999.0);
-        jobList.add(j2);
-
-        Job j3 = new Job(0, "This is the first job", null, 0, "job1", "Somewhere", LocalDateTime.now().plusHours(1), 20.0);
-        jobList.add(j3);
-
-        Job j4 = new Job(0, "This is the second job", null, 0, "job2", "Here", LocalDateTime.now().plusHours(1), 999.0);
-        jobList.add(j4);
-
-        Job j5 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
-        jobList.add(j5);
-
-        Job j6 = new Job(0, "This is the second job", null, 0, "job2", "There", LocalDateTime.now().plusHours(1), 999.0);
-        jobList.add(j6);
-
-        Job j7 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
-        jobList.add(j7);
-
-        Job j8 = new Job(0, "This is the second job", null, 0, "job2", "Anywhere", LocalDateTime.now().plusHours(1), 999.0);
-        jobList.add(j8);
-
-        Job j9 = new Job(0, "This is the first job", null, 0, "job1", null, LocalDateTime.now().plusHours(1), 20.0);
-        jobList.add(j9);
-
-        Job j10 = new Job(0, "This is the second job", null, 0, "job2", null, LocalDateTime.now().plusHours(1), 999.0);
-        jobList.add(j10);
-
         adapter.notifyDataSetChanged();
     }
 }
---
+++ src/app/src/main/java/com/example/biddlr/JobCreationFragment.java
@@ -0,0 +1,25 @@
+package com.example.biddlr;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class JobCreationFragment extends DialogFragment {
+    public static JobCreationFragment newInstance() {
+        JobCreationFragment fragment = new JobCreationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_job_creation, container, false);
+    }
+}
