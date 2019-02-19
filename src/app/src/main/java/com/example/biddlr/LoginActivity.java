db22abeff76ced8c66b2a2397ef65109976c73d0
faaljohani <faljohani@crimson.ua.edu>
2019-02-19 05:57:51 -0600

    Hard code one user for the demo and login validation.

--- src/app/src/main/java/com/example/biddlr/LoginActivity.java
+++ src/app/src/main/java/com/example/biddlr/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,30 +18,59 @@ public class LoginActivity extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent nextScreen = new Intent(v.getContext(), HomeActivity.class);
-                startActivityForResult(nextScreen, 0);
+            public void onClick(View v) {
+                actionLogin();
             }
         });
 
-
-//create pass
         Button btnCreateUser = findViewById(R.id.btnCreateUser);
         btnCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent nextScreen = new Intent(v.getContext(), CreateUserActivity.class);
-                startActivityForResult(nextScreen, 0);
+            public void onClick(View v) {
+                actionCreateUser();
             }
         });
 
         Button btnForgetPass = findViewById(R.id.btnForgetPass);
         btnForgetPass.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent nextScreen = new Intent(v.getContext(), ForgetPassActivity.class);
-                startActivityForResult(nextScreen, 0);
+            public void onClick(View v) {
+                actionForgetPass();
             }
         });
     }
+
+    private void actionLogin() {
+        UtilityInterfaceTools.hideSoftKeyboard(LoginActivity.this);
+        EditText txtEmail = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+
+        if (txtEmail.getText().toString().equals("jsmith@ua.edu") && txtPassword.getText().toString().equals("testtest123")) {
+            if (true) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                finish();
+            }
+        }
+
+            else {
+                TextView errorMessage = findViewById(R.id.message_invalid_credentials);
+                errorMessage.setVisibility(View.VISIBLE);
+                txtEmail.setText("");
+                txtPassword.setText("");
+            }
+
+    }
+
+    private void actionCreateUser() {
+        UtilityInterfaceTools.hideSoftKeyboard(LoginActivity.this);
+        startActivity(new Intent(LoginActivity.this, CreateUserActivity.class));
+       // finish();
+    }
+
+    private void actionForgetPass() {
+        UtilityInterfaceTools.hideSoftKeyboard(LoginActivity.this);
+        startActivity(new Intent(LoginActivity.this, ForgetPassActivity.class));
+       // finish();
+    }
 }
+
