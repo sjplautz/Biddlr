32e108742280d73cbb4e9ed692615c37c5d6d27c
faaljohani <faljohani@crimson.ua.edu>
2019-02-19 06:46:48 -0600

    Update forgot password class.

--- src/app/src/main/java/com/example/biddlr/ForgetPassActivity.java
+++ src/app/src/main/java/com/example/biddlr/ForgetPassActivity.java
@@ -1,15 +1,14 @@
 package com.example.biddlr;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ForgetPassActivity extends AppCompatActivity {
 
@@ -17,8 +16,32 @@ public class ForgetPassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpass);
+        Button buttonSubmit = (Button) findViewById(R.id.btnSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                actionForgetPass();
+            }
+        });
+    }
 
+    private void actionForgetPass(){
+        EditText inputEmail = (EditText) findViewById(R.id.txtEnEmail);
+        String sEmail = inputEmail.getText().toString();
+        AlertDialog.Builder b = new AlertDialog.Builder(ForgetPassActivity.this);
+        b.setCancelable(true);
+        b.setTitle("Check Your Inbox");
+        b.setMessage("A reset password link has been sent to " + sEmail + ".");
+        b.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(ForgetPassActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                });
 
-//test
+        AlertDialog confirmationDialog = b.create();
+        confirmationDialog.show();
     }
 }
