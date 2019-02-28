15e5b41e77ba548ae12af42f00ed5a36b4c3068b
faaljohani <faljohani@crimson.ua.edu>
2019-02-28 02:49:13 -0600

    finished splash activity class

--- src/app/src/main/java/com/example/biddlr/SplashActivity.java
+++ src/app/src/main/java/com/example/biddlr/SplashActivity.java
@@ -1,4 +1,29 @@
 package com.example.biddlr;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000; //This is 4 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        //Code to start timer and take action after the timer ends
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity( new Intent(SplashActivity.this, LoginActivity.class));
+
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
 }
+
