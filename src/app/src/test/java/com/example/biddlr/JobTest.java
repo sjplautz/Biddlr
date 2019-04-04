9159003f59dbb13956b823cd2fe1d22267cddd9a
Trevor Roest <twroest@crimson.ua.edu>
2019-04-03 23:58:44 -0500

    Removed bad test

--- src/app/src/test/java/com/example/biddlr/JobTest.java
+++ src/app/src/test/java/com/example/biddlr/JobTest.java
@@ -1,18 +1,10 @@
 package com.example.biddlr;
 
-import android.os.Parcel;
-
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-
-import adapters.JobListAdapter;
 import classes.Job;
-import classes.LatLngWrapped;
 import classes.LocalDateTimeWrapped;
-import enums.JobStatus;
 
 import static org.junit.Assert.*;
 
@@ -66,30 +58,5 @@ public class JobTest {
         assertEquals(job1, job2);
     }
 
-    @Test
-    public void writeToParcel_isCorrect(){
-        Job job1 = new Job();
-        job1.setJobID("ID");
-        job1.setStatus(JobStatus.IN_PROGRESS);
-        job1.setTitle("Title");
-        job1.setDescription("Desc");
-        job1.setPosterID("PosterID");
-        job1.setSelectedBidderID("BidderID");
-        job1.setLocation("Here");
-        job1.setCoordinates(new LatLngWrapped(1.0, 1.0));
-        job1.setExpirationDate(new LocalDateTimeWrapped(LocalDateTime.now()));
-        job1.setStartingPrice(30.0);
-        job1.setCurrentBid(20.0);
-        HashMap<String, Double> bids = new HashMap<>();
-        bids.put("BidderID", 20.0);
-        job1.setBids(bids);
 
-        Parcel p = Parcel.obtain();
-        job1.writeToParcel(p, 0);
-
-        Job job2 = (Job) Job.CREATOR.createFromParcel(p);
-
-        assertEquals("ID", job2.getJobID());
-        assertEquals("Here", job2.getLocation());
-    }
 }
