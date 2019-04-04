7b3b4eb6819e7a35f65f22668f0aef445088baf4
Trevor Roest <twroest@crimson.ua.edu>
2019-04-03 19:03:02 -0500

    Added tests

--- src/app/src/test/java/com/example/biddlr/JobTest.java
+++ src/app/src/test/java/com/example/biddlr/JobTest.java
@@ -1,12 +1,18 @@
 package com.example.biddlr;
 
+import android.os.Parcel;
+
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 import adapters.JobListAdapter;
 import classes.Job;
+import classes.LatLngWrapped;
 import classes.LocalDateTimeWrapped;
+import enums.JobStatus;
 
 import static org.junit.Assert.*;
 
@@ -29,7 +35,8 @@ public class JobTest {
         Job job = new Job();
 
         //Sets expiration date to Jan 1 2019
-        job.setExpirationDate(new LocalDateTimeWrapped(LocalDateTime.from));
+        job.setExpirationDate(new LocalDateTimeWrapped(LocalDateTime.of(2019, 1, 2, 0, 0)));
+        assertEquals("Jan 2, 2019 12:00:00 AM", job.formattedExpirationDate());
     }
 
     @Test
@@ -47,4 +54,42 @@ public class JobTest {
         assertEquals("2 days", job.formattedTimeFromNow());
     }
 
+    @Test
+    public void equals_isCorrect(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1.setJobID("TestID");
+        job2.setJobID("TestID");
+
+        assertNotSame(job1, job2);
+        assertEquals(job1, job2);
+    }
+
+    @Test
+    public void writeToParcel_isCorrect(){
+        Job job1 = new Job();
+        job1.setJobID("ID");
+        job1.setStatus(JobStatus.IN_PROGRESS);
+        job1.setTitle("Title");
+        job1.setDescription("Desc");
+        job1.setPosterID("PosterID");
+        job1.setSelectedBidderID("BidderID");
+        job1.setLocation("Here");
+        job1.setCoordinates(new LatLngWrapped(1.0, 1.0));
+        job1.setExpirationDate(new LocalDateTimeWrapped(LocalDateTime.now()));
+        job1.setStartingPrice(30.0);
+        job1.setCurrentBid(20.0);
+        HashMap<String, Double> bids = new HashMap<>();
+        bids.put("BidderID", 20.0);
+        job1.setBids(bids);
+
+        Parcel p = Parcel.obtain();
+        job1.writeToParcel(p, 0);
+
+        Job job2 = (Job) Job.CREATOR.createFromParcel(p);
+
+        assertEquals("ID", job2.getJobID());
+        assertEquals("Here", job2.getLocation());
+    }
 }
---
+++ src/app/src/test/java/com/example/biddlr/UserTest.java
@@ -0,0 +1,13 @@
+package com.example.biddlr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import classes.User;
+
+public class UserTest {
+
+}
