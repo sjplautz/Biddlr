9e59b8119cf029566a2fdbabb5d9344bf3b6df9d
faaljohani <faljohani@crimson.ua.edu>
2019-04-04 06:57:57 -0500

    Added comments for forget password activity class

--- src/app/src/main/java/ui/ForgotPassActivity.java
+++ src/app/src/main/java/ui/ForgotPassActivity.java
@@ -23,31 +23,30 @@ import java.util.Objects;
 import classes.DatabaseManager;
 
 
-
+//Forget password activity class
 
 public class ForgotPassActivity extends AppCompatActivity {
 
 
     private EditText inputEmail;
-
     protected Button btnReset ;
     private ProgressBar progressBar;
     private ImageView imglogo;
 
-    //private FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Reset Password");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_forgetpass);
-       // getSupportActionBar()//.hide();
+
         progressBar = findViewById(R.id.progressBar);
         imglogo= findViewById(R.id.imglogo);
         inputEmail = findViewById(R.id.txtEnEmail);
         btnReset= findViewById(R.id.btnSubmit);
-       // auth = FirebaseAuth.getInstance();
+
         btnReset.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("WrongConstant")
             @Override
@@ -55,12 +54,15 @@ public class ForgotPassActivity extends AppCompatActivity {
 
                 String email = inputEmail.getText().toString().trim();
 
+                //Validate the email field
                 if (TextUtils.isEmpty(email)) {
 
                     Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_LONG).show();
 
                     return;
                 }
+
+                //Handle the visibility of the progressBar,btnReset,inputEmail and imglogo
                 progressBar.setVisibility(View.VISIBLE);
                 btnReset.setVisibility(View.INVISIBLE);
                 inputEmail.setVisibility(View.INVISIBLE);
@@ -69,17 +71,19 @@ public class ForgotPassActivity extends AppCompatActivity {
                 DatabaseManager.shared.mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
+                                // Action process for reset password if email is valid
                                 if (task.isSuccessful()) {
 
                                     Toast.makeText(ForgotPassActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_LONG).show();
                                     startActivity(new Intent(ForgotPassActivity.this,LoginActivity.class));
                                 }
 
-
+                                // Display an error msg if email is not valid
                                 else {
                                     Toast.makeText(ForgotPassActivity.this, "Failed to send reset email!", Toast.LENGTH_LONG).show();
 
                                 }
+                                //Handle the visibility of the progressBar,btnReset,inputEmail and imglogo
                                 progressBar.setVisibility(View.GONE);
                                 imglogo.setVisibility(View.INVISIBLE);
                                 btnReset.setVisibility(View.VISIBLE);
