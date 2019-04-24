ccc6e55451f9e9e28dc76dc220c79bb7b588ae4f
faaljohani <faljohani@crimson.ua.edu>
2019-04-24 17:13:23 -0500

    Added some comments

--- src/app/src/main/java/classes/DatabaseManager.java
+++ src/app/src/main/java/classes/DatabaseManager.java
@@ -4,9 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -30,7 +27,6 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -41,7 +37,6 @@ import java.util.Map;
 import enums.JobStatus;
 import interfaces.JobDataListener;
 import interfaces.UserDataListener;
-import ui.MessageFragment;
 
 public class DatabaseManager {
 
@@ -210,6 +205,10 @@ public class DatabaseManager {
             public void onCancelled(@NonNull DatabaseError databaseError) { }
         });
     }
+
+    /**
+     * All jobs completed by a user
+     */
 
     public void setCompletedJobsForBidderListener(String userID, final JobDataListener listener) {
         Log.d("COMPLETED_OUTER", "Query was called at least");
